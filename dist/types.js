c2093cf5a2cbe799441d8246a617917a3936ec8f
pritesh <pritesh.v.bachhav.cg@gmail.com>
2026-09-16 10:57:57 +0530

    Stop tracking dist files

--- dist/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.assertNever = assertNever;
-// This function helps check all cases
-function assertNever(value) {
-    throw new Error(`Unhandled value: ${value}`);
-}
